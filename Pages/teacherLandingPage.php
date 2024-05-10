f0e8a11684b03afedddb4a0dc4e85b2c3e0e9983
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-05-10 16:33:42 +0200

    small changes

--- Pages/teacherLandingPage.php
+++ Pages/teacherLandingPage.php
@@ -60,6 +60,7 @@ ob_start();
         <section id="progress">
             <h2>Elevprogression</h2>
             <p> Laddar dagens progression..</p>
+            <svg fill="#000000" viewBox="0 0 24 24" id="diagram-bar" data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" class="icon flat-line"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="primary" d="M19,15v2m-8-4v4m4-6v6M7,9v8" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-2" data-name="primary" d="M3,3V20a1,1,0,0,0,1,1H21" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>
             <div>
                 <canvas id="progressChart"></canvas>
             </div>
