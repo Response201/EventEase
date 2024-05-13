0355755a45a39bc1d43768c98c7f75e047d2f0d7
Marcus Gustavsson <mackan1892@outlook.com>
2024-05-13 15:08:14 +0200

    fix

--- contact.js
+++ contact.js
@@ -1,12 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var teacherInfos = document.querySelectorAll(".teacher-container-info");
-//     teacherInfos.forEach(function(info, index) {
-//         setTimeout(function() {
-//             info.classList.add("show");
-//         }, 1900 * (index + 1)); // Lägger till en ökande fördröjning för varje element
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
     var teacherInfos = document.querySelectorAll(".teacher-container-info");
 
@@ -17,18 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
         teacherInfos.forEach(function(info, index) {
             var elementOffsetTop = info.getBoundingClientRect().top;
 
-            // Om elementet är synligt på sidan
+
             if (elementOffsetTop - windowHeight < 0) {
                 setTimeout(function() {
                     info.classList.add("show");
-                }, 200 * (index + 1)); // Lägger till en ökande fördröjning för varje element
+                }, 200 * (index + 1)); 
             }
         });
     }
 
-    // Lyssna på scrollhändelsen och anropa checkScroll-funktionen
+    
     window.addEventListener("scroll", checkScroll);
 
-    // Kolla scrollpositionen när sidan laddas också
+  
     checkScroll();
 });
