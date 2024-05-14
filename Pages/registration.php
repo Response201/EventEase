47c9585a0865efc0867c9aad402e701fa21f3b4d
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-05-14 10:52:43 +0200

    fix

--- Pages/registration.php
+++ Pages/registration.php
@@ -4,13 +4,14 @@ require_once ('Models/Database.php');
 require_once ('functions/auth.php');
 require_once ("Utils/Validator.php");
 
+
 $v = new Validator($_POST);
 $dbContext = new DBContext();
 $message = "";
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $email = $_POST['email'] ?? '';
-$invalidPasswordMessage = "";
+
 
 if (isset($_POST['create'])) {
 
@@ -19,26 +20,18 @@ if (isset($_POST['create'])) {
     } else {
         $v->field('email')->required()->email()->min_val(1)->max_len(100);
         $v->field('password')->required()->match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/");
-
-
-        $email_valid = $v->field('email')->is_valid();
-        $password_valid = $v->field('password')->is_valid();
-
-        if ($email_valid && $password_valid) {
+        if ($v->is_valid()) {
             $message = auth();
-            if ($message === 'Perfekt, kolla mailet och verifiera ditt konto') {
+            if ($message === 'Prefekt, kolla mailet och verifiera ditt konto') {
                 $dbContext->getUsersDatabase()->makeConsumer($email);
             }
         } else {
-            if (!$password_valid) {
-                $invalidPasswordMessage = "minimum 6 tecken, minst en stor bokstav och ett special tecken";
-            } else {
-                $message = "Det gick inte registrera kontot";
-            }
+            $message = "Det gick inte registrera kontot";
         }
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,6 +39,7 @@ if (isset($_POST['create'])) {
 <head>
     <title>Registration</title>
     <?php include (__DIR__ . '/../includes/head.php'); ?>
+
 </head>
 
 <body>
@@ -64,13 +58,13 @@ if (isset($_POST['create'])) {
                 <input type="text" id="username" name="username" required>
                 <br>
                 <label for="password">Lösenord:</label>
+                <label>minimum 6 tecken, minst
+                    en stor bokstav och ett special tecken</label>
                 <input type="password" id="password" name="password" required>
-                <?php if ($invalidPasswordMessage): ?>
-                    <span style="color:red;"><?php echo $invalidPasswordMessage; ?></span>
-                <?php endif; ?>
+
                 <br>
                 <button type="submit" name="create">Registrera</button>
-                <p><?php echo $message; ?></p>
+                <p><?php echo "$message"; ?></p>
             </form>
             <p> Har du redan ett konto? <a href="/login">Logga in här!</a></p>
         </div>
