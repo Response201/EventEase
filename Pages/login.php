218c792e885e20363f89bed65b26b8e59e3eb175
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-05-14 09:48:50 +0200

    fix

--- Pages/login.php
+++ Pages/login.php
@@ -1,35 +1,56 @@
 <?php
-/* session_start(); */
+
 ob_start();
 
 include_once ("Models/Database.php");
 
+
+
 $message = $message = $_GET['message'] ?? "";
+
 $dbContext = new DBContext();
 
 
 
-try {
-    $dbContext->getUsersDatabase()->getAuth()->login($username, $password);
+$username = $_POST['username'] ?? '';
 
-    if ($dbContext->getUsersDatabase()->getAuth()->hasRole(\Delight\Auth\Role::AUTHOR)) {
-        header("Location: /meeting");
-    } else if ($dbContext->getUsersDatabase()->getAuth()->hasRole(\Delight\Auth\Role::CONSUMER)) {
-        header("Location: /"); // Omdirigera användare till startsidan
+$password = $_POST['password'] ?? '';
+
+if (isset($_POST['login']) && $username && $password) {
+
+    try {
+
+        $dbContext->getUsersDatabase()->getAuth()->login($username, $password);
+
+        if ($dbContext->getUsersDatabase()->getAuth()->hasRole(\Delight\Auth\Role::AUTHOR)) {
+
+            header("Location: /meeting");
+
+        } else if ($dbContext->getUsersDatabase()->getAuth()->hasRole(\Delight\Auth\Role::CONSUMER)) {
+
+            header("Location: /"); // Omdirigera användare till startsidan
+
+        }
+
+    } catch (\Throwable $th) {
+
+        $message = "något gick fel";
 
     }
-} catch (\Throwable $th) {
-    $message = "något gick fel";
+
 
 }
 
-
 ?>
+
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
+
     <title>Login</title>
+
     <?php include (__DIR__ . '/../includes/head.php'); ?>
 
 </head>
@@ -37,28 +58,48 @@ try {
 <body>
 
     <?php include ('./views/Navbar.php'); ?>
+
     <div class="login-wrapper">
+
         <div class="login-container">
+
             <h2>Logga In</h2>
+
             <form method="POST">
+
                 <label for="username">Användarnamn:</label>
+
                 <input type="text" id="username" name="username" required>
+
                 <br>
+
                 <label for="password">Lösenord:</label>
+
                 <input type="password" id="password" name="password" required>
+
                 <br>
+
                 <button type="submit" name="login">Logga In</button>
+
                 <p><?php echo "$message"; ?></p>
+
             </form>
 
-            <p> Har du inget konto? <a href="/registration">Registrera dig här!</a></p>
+            <p> Har du inget konto? <a href="/register">Registrera dig här!</a></p>
+
+        </div>
+
+        <div class="login-slogan-eventease">
+
+            <h2>EventEase </h2>
+
         </div>
 
     </div>
 
 
-
     <?php include (__DIR__ . '/../views/Footer.php'); ?>
+
 </body>
 
 </html>
