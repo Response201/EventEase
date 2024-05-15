f64a42dd8681217bd60b2d9b10f4f4aef92cab73
Mercedesemiliea <mercedesemiliea@hotmail.se>
2024-05-15 11:10:46 +0200

    img

--- Pages/index.php
+++ Pages/index.php
@@ -13,6 +13,7 @@ include_once ("Models/Database.php");
 <div class="slogan-text"> <h1> EventEase</h1>
 <p>Studera smartare, inte hårdare – Anmäl dig till handledning idag!</p></div>
 <div class="landingpage-background-container"> </div>
+
 </div>
 <!--
      <?php foreach ($teacherIds as $row): ?>
